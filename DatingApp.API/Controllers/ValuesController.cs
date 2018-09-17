e87290e757220b7369d546f538dfff835763cab3
Corbin Seghers <corbin.seghers@choosebroadspire.com>
2018-09-17 13:16:29 -0500

    End of Section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -16,12 +16,14 @@ namespace DatingApp.API.Controllers
     public class ValuesController : ControllerBase
     {
         private readonly DataContext _context;
+        
         public ValuesController(DataContext context)
         {
             this._context = context;
         }
 
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
